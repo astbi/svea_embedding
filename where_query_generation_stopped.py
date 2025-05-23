0c6141b703184bf23b1e67b69d3523214708eaca
astbi <168831619+astbi@users.noreply.github.com>
2025-05-23 09:11:37 +0200

    Comments

--- where_query_generation_stopped.py
+++ where_query_generation_stopped.py
@@ -3,29 +3,25 @@ import os
 import sys
 from tqdm import tqdm
 
-htr_dir = sys.argv[1]
-endtext = sys.argv[2]
+htr_dir = sys.argv[1] # directory with HTR volumes
+endtext = sys.argv[2] # last text string processed
 dirlist = os.listdir(htr_dir)
 
-checkpoint = False
-
 for subdir in tqdm(dirlist):
-    the_text = ""
+    the_text = "" # For gathering text
     subdir_path = os.path.join(htr_dir, subdir)
     for filename in os.listdir(subdir_path):
-        if checkpoint:
-            if filename.endswith(".json"):
-                filepath = os.path.join(subdir_path, filename)
-                with open(filepath, "r", encoding="utf-8") as file:
-                    data = json.load(file)
-                try:
-                    for part in data["contains"]:
-                        for text in part["contains"]:
-                            the_text += " ".join(text["text_result"]["texts"])+" "
-                except KeyError:
-                    pass
-                if endtext in the_text:
-                    print("Here:", filepath)
-                    exit()
-        if filename == "A0068689_00710.json":
-            checkpoint = True
+        if filename.endswith(".json"):
+            filepath = os.path.join(subdir_path, filename)
+            with open(filepath, "r", encoding="utf-8") as file:
+                data = json.load(file)
+            try:
+                for part in data["contains"]:
+                    for text in part["contains"]:
+                        # Add the text from the file
+                        the_text += " ".join(text["text_result"]["texts"])+" "
+            except KeyError:
+                pass
+            if endtext in the_text: # We found the text
+                print("Here:", filepath)
+                exit()
